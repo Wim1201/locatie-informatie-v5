a769a6cb19109217d8568f4c616e589b4d6591e4
Wim1201 <wim.snoeren@me.com>
2025-04-12 13:40:17 +0200

    config.py toegevoegd

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+# API-sleutels ophalen uit omgevingsvariabelen
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+BAG_API_KEY = os.getenv("BAG_API_KEY")
+RUIMTELIJKE_PLANNEN_API_KEY = os.getenv("Ruimtelijke_plannen_(V4)_API_KEY")
+import os
+
+
+
